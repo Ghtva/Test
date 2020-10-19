e88a259e0142d66f1cfbbf5822345e47036323f8
Anuar Orazgaliyev <anikxx061@gmail.com>
2020-10-19 18:02:42 +0600

    Test1

--- index.php
+++ index.php
@@ -26,10 +26,6 @@
     <label>Ваш логин:<br></label>
     <input name="login" type="text" size="15" maxlength="15">
     </p>
-<p>
-    <label>Ваш пароль:<br></label>
-    <input name="password" type="password" size="15" maxlength="15">
-    </p>
 
 <p>
     <input type="submit" name="submit" value="Зарегистрироваться">
