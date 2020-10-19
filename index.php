267f9e9df58fcd80df7836c8cda5d78658a9ee27
Anuar Orazgaliyev <anikxx061@gmail.com>
2020-10-19 17:51:43 +0600

    Test

---
+++ index.php
@@ -0,0 +1,43 @@
+
+
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>Интернет-магазин товаров для дома</title>
+    <link href="style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <header class="site-header">
+      <nav class="site-navigation">
+        <a class="logo-link" href="index.php">
+        </a>
+        <ul class="navigation-list">
+          <li><a href="catalog.php">Каталог</a></li>
+          <li><a href="delivery.html">Доставка</a></li>
+          <li><a href="contacts.html">Контакты</a></li>
+         <head>
+    <title>Регистрация</title>
+    </head>
+    <body>
+    <h2>Регистрация</h2>
+    <form action="save_user.php" method="post">
+<p>
+    <label>Ваш логин:<br></label>
+    <input name="login" type="text" size="15" maxlength="15">
+    </p>
+<p>
+    <label>Ваш пароль:<br></label>
+    <input name="password" type="password" size="15" maxlength="15">
+    </p>
+
+<p>
+    <input type="submit" name="submit" value="Зарегистрироваться">
+</p></form>
+    </body>  
+        </ul>
+      </nav>
+    </header>
+    </html>
+    
